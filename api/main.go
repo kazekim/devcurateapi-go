c63bdeb79d91f2b72aa8c6e1e9febddfdcb99897
Jirawat Harnsiriwatanakit <jirawat.h@inception.asia>
2019-09-02 11:48:24 +0700

    fix dockerfile

--- api/main.go
+++ api/main.go
@@ -4,6 +4,33 @@
 
 package main
 
-func main() {
+import (
+	"github.com/kazekim/devcurateapi-go/api/route"
+	"log"
+	"github.com/gorilla/mux"
+	"gopkg.in/mgo.v2"
+	"net/http"
+	"os"
+	"github.com/rs/cors"
+)
 
+
+func main() {
+	// Connect to mongo
+	session, err := mgo.Dial("mongo:27017")
+	if err != nil {
+		log.Fatalln(err)
+		log.Fatalln("mongo err")
+		os.Exit(1)
+	}
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+
+	// Set up routes
+	r := mux.NewRouter()
+	r.HandleFunc("/health_check", route.HealthCheck).
+		Methods("GET")
+
+	http.ListenAndServe(":8080", cors.AllowAll().Handler(r))
+	log.Println("Listening on port 8080...")
 }
---
+++ api/route/healthcheck.go
@@ -0,0 +1,14 @@
+/*
+  GoLang code created by Jirawat Harnsiriwatanakit https://github.com/kazekim
+*/
+
+package route
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("ssssssss")
+}
